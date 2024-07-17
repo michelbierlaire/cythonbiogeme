352a6c4ff33df2d9ac9b3096c86debc99d871d6d
Michel Bierlaire <michel.bierlaire@epfl.ch>
2024-07-17 12:02:00 +0200

    Update bioDebug.h

--- src/cythonbiogeme/cpp/bioDebug.h
+++ src/cythonbiogeme/cpp/bioDebug.h
@@ -7,14 +7,15 @@
 //
 //--------------------------------------------------------------------
 
-#ifndef bioDebug_h
-#define bioDebug_h
+    #ifndef bioDebug_h
+    #define bioDebug_h
 
-#include <iostream>
+    #include <iostream>
 
-#ifdef DEBUG
-#define DEBUG_MESSAGE(message) {std::cout << __FILE__ << ":" << __LINE__ << " " << message << std::endl ; }
-#else
-#define DEBUG_MESSAGE(message) {;}
-#endif
-#endif
+    #ifdef DEBUG
+    #define DEBUG_MESSAGE(message) do { std::cout << __FILE__ << ":" << __LINE__ << " " << message << std::endl; } while (0)
+    #else
+    #define DEBUG_MESSAGE(message) do {} while (0)
+    #endif
+
+    #endif
